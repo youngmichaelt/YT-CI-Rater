20912914f81f5484c2d75c3eb366faf0f965f4a7
Mac <mac@Macs-MacBook.local>
2021-10-25 19:32:22 -0400

    Updating functionality to score videos

--- classify.py
+++ classify.py
@@ -3,6 +3,7 @@ from os import path
 from pywordseg import *
 
 def create_hsk_dict():
+    print("Loading HSK files into Dictionary")
     HSK = os.getcwd() + "/HSK/"
     hsk_dict = {}
     for file in os.listdir(HSK):
@@ -26,7 +27,7 @@ def create_hsk_dict():
                 line = line.strip()
                 hsk_dict[line] = hsk_num
 
-    print(hsk_dict)
+    # print(hsk_dict)
     return hsk_dict
 
 def create_vid_dict(hsk_dict):
@@ -34,52 +35,57 @@ def create_vid_dict(hsk_dict):
     vid_dicts = []
     text_file_paths = '/Users/mac/Desktop/repos/temp-YT-CI/text_files/'
     seg = Wordseg(batch_size=64, embedding='w2v', mode="TW")
+    files = []
     for file in os.listdir(text_file_paths):
-        with open(text_file_paths + file) as vid_file:
-            vid_dict = {}
-            lines = vid_file.readlines()
-            lines = [line.rstrip() for line in lines]
-            res = seg.cut(lines)
-            print(res)
-            for char in res[0]:
-                
-                char = char.strip()
-
-                if char in hsk_dict:
-                    vid_dict[char] = hsk_dict[char]
-                else: 
-                    vid_dict[char] = 0
-
+        if file.split('.')[1] == "txt":
+            files.append(file)
+            print("Loading video captions:" + file)
+            with open(text_file_paths + file) as vid_file:
+                vid_dict = {}
+                lines = vid_file.readlines()
+                lines = [line.rstrip() for line in lines]
+                res = seg.cut(lines)
+                # print(res)
+                for char in res[0]:
                     
-            vid_dicts.append(vid_dict)
-    
-    print(vid_dicts)
-    return vid_dicts
+                    char = char.strip()
 
-def scores(vid_dicts):
+                    if char in hsk_dict:
+                        vid_dict[char] = hsk_dict[char]
+                    else: 
+                        vid_dict[char] = 0
+
+                        
+                vid_dicts.append(vid_dict)
+    
+    # print(vid_dicts)
+    return vid_dicts, files
+
+def scores(vid_dicts, files):
     scores = {}
     counter = 0
     for vid_dict in vid_dicts:
         for key, value in vid_dict.items():
-            if counter in scores:
-                scores[counter] += value
+            if files[counter] in scores:
+                scores[files[counter]] += value
             else:
-                scores[counter] = value
+                scores[files[counter]] = value
+        scores[files[counter]] = scores[files[counter]] / len(vid_dict)
         counter += 1
     
-    print(scores)
+    print("Scores: ")
+
+    for key,value in sorted(scores.items(), key=lambda item: item[1]):
+        print()
+        print(key, value)
+    
 
     
 
-
-
-
-
-
-
-    pass
+def main():
+    hsk = create_hsk_dict()
+    vids, files = create_vid_dict(hsk)
+    scores(vids, files)
 
 if __name__ == "__main__":
-    hsk = create_hsk_dict()
-    vids = create_vid_dict(hsk)
-    scores(vids)
+    main()
--- download.py
+++ download.py
@@ -15,8 +15,8 @@ class Logger(object):
     def error(self, msg):
         print(msg)
 
-def download_captions(code):
-    code = "oaA5N6Wso_o"
+def download_captions(code, title):
+    # code = "ChCJmbW2d_8"
     try:
         transcript_list = YouTubeTranscriptApi.list_transcripts(code)
         print(transcript_list)
@@ -26,7 +26,7 @@ def download_captions(code):
         for x in srt:
             sentences.append(x['text'])
 
-        f = open("/Users/mac/Desktop/repos/temp-YT-CI/text_files/" + code +".txt", "w")
+        f = open("/Users/mac/Desktop/repos/temp-YT-CI/text_files/" + title +".txt", "w")
         for sentence in sentences:
             for x in sentence.split():
                 f.write(x)
@@ -48,7 +48,7 @@ def download_wav(code):
             'preferredquality': '192',
             
         }],
-        'outtmpl': '/Users/mac/Desktop/repos/temp-YT-CI/wav_files/%(title)s.%(ext)s'
+        'outtmpl': '/Users/mac/Desktop/repos/temp-YT-CI/temp_wav/%(title)s.%(ext)s'
         # 'no_check_certificate': True,
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -59,7 +59,7 @@ def download_wav(code):
 
 def recognize(file_path):
     r = sr.Recognizer()
-    wav_file_path = file_path + "wav_files/"
+    wav_file_path = file_path + "temp_wav/"
     text_file_path = file_path + "text_files/"
 
     wav_files = []
@@ -69,17 +69,22 @@ def recognize(file_path):
         wav_files.append(file)
 
     for file in wav_files:
-        with sr.AudioFile(wav_file_path + file) as source:
-            audio = r.record(source)  
-        result = r.recognize_google(audio, language = 'zh').lower()
+        if file.split('.')[1] == "wav":
+            with sr.AudioFile(wav_file_path + file) as source:
+                audio = r.record(source)  
+            result = r.recognize_google(audio, language = 'zh').lower()
 
-        print(result)
-        f = open(text_file_path + file.split('.')[0]+".txt", "w")
-        f.writelines(result)
-        f.close()
+            print(result)
+            f = open(text_file_path + file.split('.')[0]+".txt", "w")
+            f.writelines(result)
+            f.close()
+            os.replace(wav_file_path + file, file_path + "wav_files/" + file)
+
+        else:
+            print("Not a WAV file...")
 
 if __name__ == "__main__":
-    download_captions("ChCJmbW2d_8")
+    download_captions("ck8Sq8qo9dY", "Comprehensible Input Chinese - Slow & Clear | How Chinese Parents Show Love | Hit Chinese TPRS")
     # recognize('/Users/mac/Desktop/repos/temp-YT-CI/')
     # download_wav("oaA5N6Wso_o")
 
